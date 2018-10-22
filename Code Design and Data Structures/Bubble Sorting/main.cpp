4c11b804201e647f827a7368188ad18c6d7f8205
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-10-22 17:57:01 -0500

    Added Bubble Sorting

---
+++ Code Design and Data Structures/Bubble Sorting/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct Player
+{
+	int m_Health;
+	void other(int Health);
+	int temp;
+};
+
+void Player::other(int Health)
+{
+	for (int i = 0; i <= Health; i++)
+	{
+		for (int j = 0; j <= Health; j++)
+		{
+			if (j < i)
+			{
+				temp = j;
+				j = i;
+				i = temp;
+				j++;
+			}
+		}
+	}
+}
+
+int main()
+{
+	Player p1{ 100 };
+	Player p2{ 55 };
+	Player p3{ 45 };
+	Player p4{ 12 };
+
+}
